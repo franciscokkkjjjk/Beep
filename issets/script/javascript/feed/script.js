9ebf539275284a14f83d839a2deef6adc012c83c
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2022-05-28 16:16:01 -0300

    main

--- issets/script/javascript/feed/script.js
+++ issets/script/javascript/feed/script.js
@@ -4,28 +4,35 @@ let menu ={
     'locationEvent01':'menu--pag',
     'event02': 'menu--pag--button',
 }
-
-console.log(menu['event01'])
 qs('.'+menu['event01']).addEventListener('click', ()=>{
 if(menu['status'] == false) {
+
         menu['status'] = true;
+
         qs('.event--menu-pag').style.opacity = 1;
         qs('.event--menu-pag').style.display = 'block';
+
         setTimeout(()=>{
+
             qs('.event--menu-pag').style.backgroundColor = '#4e4e4e';
             qs('.event--menu-pag').style.paddingBottom = "290px";
             qs('.eventscript--seta').classList.add('dimensoes--seta');
 
         },30)
 } else {
+
         menu['status'] = false;
+
         qs('.event--menu-pag').style.backgroundColor = '#4e4e4e';
         qs('.event--menu-pag').style.paddingBottom = "";
         qs('.eventscript--seta').classList.remove('dimensoes--seta');
+
         setTimeout(()=>{
+
             qs('.event--menu-pag').style.opacity = 0;
             qs('.event--menu-pag').style.display = 'none';
             qs('.'+menu['locationEvent01']).classList.add(menu['event01']);
-        },100)
+            
+        }, 100)
 }
 }); 
