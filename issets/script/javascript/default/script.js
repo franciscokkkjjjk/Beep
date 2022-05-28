33d490b5fa71b774fab8d608462d6f4e7a5b532e
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2022-05-28 14:02:48 -0300

    main

--- issets/script/javascript/default/script.js
+++ issets/script/javascript/default/script.js
@@ -4,31 +4,3 @@ const qs = (e) => {
 const qsAll = (e) => {
     return document.querySelectorAll(e);
 }
-menu = false;
-qs('.menu--button').addEventListener('click', ()=>{
-    if(!menu){
-        menu = true;
-        qs('.menu--header--body').style.display = 'block';
-        qs('.menu--header--body').style.opacity = '1';
-        qs('.seta--menu').style.transform = 'rotate(134deg)'
-        qs('.seta--menu').style.marginTop = '4px';
-        setTimeout(() => {
-           let a = qsAll('.menu--header--opt');//style.padding = '20px 10px';
-           a.forEach(element => {
-             element.style.padding = '20px 70px';  
-           });
-        }, 50);
-    } else {
-        menu = false;
-        let a = qsAll('.menu--header--opt');//style.padding = '20px 10px';
-            qs('.seta--menu').style.transform= '';
-            qs('.seta--menu').style.marginTop='';
-           a.forEach(element => {
-             element.style.padding = '0px 70px';  
-           });
-        setTimeout(() => {
-            qs('.menu--header--body').style.opacity = '0';
-            qs('.menu--header--body').style.display = '';
-        }, 300);
-    }
-})
---
+++ issets/script/javascript/feed/script.js
@@ -0,0 +1,22 @@
+let menu ={
+    'status':false,
+    'event01':'.menu--pag--event01',
+    'event02': '',
+}
+
+console.log(menu['event01'])
+if(menu['status'] == false) {
+    qs(menu['event01']).addEventListener('click', ()=>{
+        
+        console.log('teste');
+
+        menu['status'] = true;
+        qs('.event--menu-pag').style.display = 'block';
+        setTimeout(()=>{//background-color: #4e4e4e;
+            qs('.event--menu-pag').style.backgroundColor = '#4e4e4e';
+            qs('.event--menu-pag').style.opacity = 1;
+            qs('.event--menu-pag').style.paddingBottom = "267px";
+            qs('.event--menu-pag').class
+        },30)
+    });
+}
