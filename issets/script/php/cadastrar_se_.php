1a9798e5565683ca248309c046654154be090e3d
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2022-06-18 04:17:36 -0300

    main

--- issets/script/php/cadastrar_se_.php
+++ issets/script/php/cadastrar_se_.php
@@ -1,16 +1,39 @@
 <?php 
     session_start();
-    $datOt = date('Y-m-d',$oldDate);
-    $datIn = strtotime($a.'/'.$b.'/'.);
-    $datOt = date('Y-m-d',$oldDate);
-    die;
-    require_once 'conecta.php';
+    require_once 'conecta.php'; //567.600.000.000.000 18 anos em milesegundos
+    $meses = [
+        'Janeiro',//0
+        'Fevereiro',
+        'Março',//2
+        'Abril',
+        'Maio',//4
+        'Junho',
+        'Julho',//6
+        'Agosto',//7
+        'Setembro',
+        'Outubro',//9
+        'Novembro',
+        'Dezembro'//10
+    ];
     $pass = md5($_POST['senha_user']);
     $email = $_POST['email_user'];
     $nome = $_POST['nome_user'];
     $dia_nas = intval($_POST['dia']);
     $mes_nas = $_POST['mes'];
     $ano_nas = intval($_POST['ano']);
+    for($i = 0;$i<12;$i++){
+        if($meses[$i] == $mes_nas){
+            echo 'kk finalmente é '.$meses[$i];
+            $mesOut = $i+1;
+            echo 'janeiro é o mes '.$mesOut;
+            break;
+        }
+    }
+    $datIn = strtotime($mesOut.'/'.$dia_nas.'/'.$ano_nas);
+    var_dump($datIn);
+    die;
+    $datOt = date('Y-m-d', $datIn);
+    die;
     var_dump($dia_nas);
     var_dump($mes_nas);
     var_dump($ano_nas);
