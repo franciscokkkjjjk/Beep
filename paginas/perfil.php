907a1f5b58539ec14130889a65758b85493f087c
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2022-06-20 21:42:46 -0300

    main

--- paginas/perfil.php
+++ paginas/perfil.php
@@ -7,9 +7,113 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../issets/style/generic/style.css">
+    <link rel="stylesheet" href="../issets/style/feed/style.css">
     <title><?= $_SESSION['nome']?>| Beep</title>
 </head>
 <body>
-    
+    <div class="feed-area">
+        <div class="menu--pag--area">
+            <div class="feed-logo-body">
+                <div class="logo--area">
+                    <div class="logo">
+                        <a href="">
+                            <img src="../issets/imgs/default/Letra-B-PNG-1.png">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="body--menu-pag">
+                <div  class="menu--pag-perfil--area">
+                    <div class="menu--pag menu--pag--event01">
+                        <div class="menu--pag--img--area">
+                        </div>
+                        <div class="menu--pag--name-perfil--area">
+                            <div class="menu--pag--name-perfil">
+                                <?=$_SESSION['nome']?>
+                            </div>
+                            <div class="menu--pag--username-perfil">
+                                <?= $_SESSION['username'] ?>
+                            </div>
+                        </div>
+                        <div class="menu--pag--button-menu--area">
+                            <div class="menu--pag--button">
+                                <div class="eventscript--seta"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class=" event--menu-pag menu--pag--opt--menu--area">
+                        <div class="menu--pag--opt">
+                            <a href="" style="color: #fff;" class="img--opt-feed img--pag--inicial menu--pag--opt--section">
+                                Pagina inicial
+                            </a>
+                            <a href=''class="img--opt-feed img--pag--jogos menu--pag--opt--section">
+                                Jogos
+                            </a>
+                            <a href="" class="img--opt-feed img--pag--solic menu--pag--opt--section">
+                              Solicitar jogo
+                            </a>
+                            <a href="perfil.php" class="img--opt-feed img--pag--perf menu--pag--opt--section">
+                                Perfil
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="timeline--area">
+            <div class="feed-header-body">
+                <h1>
+                    Inicio
+                </h1>
+            </div>
+            <div class="feed-body-post">
+                <div class="">
+                    <!-- post direto parecido com o que tem no facebook e no twitter(rever)-->
+                </div>
+                <div class="post--menu--area">
+                    <div class="header--post--area">
+                        <div class="post--area--perfil">
+                            a
+                        </div>
+                        <div class="post--area--menu">
+                            ...
+                        </div>                                                              <!--deve ter o nome e @ do usuario e o menu de denuncia de cada usuario-->
+                    </div>
+                    <div class="body--post--area">
+                        --                                                              <!--deve ter oq o usuario publicou-->
+                    </div>
+                    <div class="interacao--post--area">
+                        --                                                              <!--deve ter o curtir compartilhar e comentar-->
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        
+        <div class="area--convite">
+            <div class="feed-logo-body menu--header">
+                <div class="menu">
+                    <div class="menu--pag--button-menu--area " >
+                        <div class="menu--pag--button button--header">
+                            <div class="event--header"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="menu--header--body">
+                    <a href='../issets/script/php/logout.php' class="opt--menu-header">
+                        <div class="img--menu--header logout"></div>
+                        <div class="text--menu--header">logout</div>
+                    </a>
+                </div>
+            </div>
+            <div class="convite--body">
+                a
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript" src="../issets/script/javascript/default/script.js"></script>
+    <script type="text/javascript" src="../issets/script/javascript/feed/script.js"></script>
 </body>
 </html>
