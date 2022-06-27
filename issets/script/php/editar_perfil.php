8015b50f50b8ccc3dc87efac72c0a8456c0bcc4c
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2022-06-27 13:04:52 -0300

    main

--- issets/script/php/editar_perfil.php
+++ issets/script/php/editar_perfil.php
@@ -11,5 +11,25 @@ $bio_user = $_POST['bio_edit'];
 $mes_user = $_POST['mes'];
 $ano_user = $_POST['ano'];
 $dia_user = $_POST['dia'];
+$meses = [
+    'Janeiro',//0
+    'Fevereiro',
+    'Março',//2
+    'Abril',
+    'Maio',//4
+    'Junho',
+    'Julho',//6
+    'Agosto',//7
+    'Setembro',
+    'Outubro',//9
+    'Novembro',
+    'Dezembro'//10
+];
+for($i = 0;$i<12;$i++){
+    if($meses[$i] == $mes_nas){
+        $mesOut = $i+1;
+        break;
+    }
+}
 //é possivel mandar funções de js pelo php $_session['funcao'] = 'funcaojs_()';
 ?>
