34ecc85034935382f6dc7c50e589839ee4f69632
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2022-07-13 00:34:37 -0300

    main

--- issets/script/javascript/feed/script.js
+++ issets/script/javascript/feed/script.js
@@ -30,12 +30,24 @@ qs('.form--inpudiv--event').addEventListener('click',()=>{
 
 function showImg_form(e){
     let img_f = document.createElement('img');
+    let exit = document.createElement('div');
+    exit.setAttribute('class', 'exit--previu');
+    let exit_img = document.createElement('div');
+    exit_img.setAttribute('class', 'menu--exit-img');
+    exit.appendChild(exit_img);
     return e.addEventListener('change', function () {   
         let img = e.files[0];
         console.log(img);
         let src = URL.createObjectURL(img);
         img_f.setAttribute('src', src);
+        qs('.img--post').appendChild(exit);
         qs('.img--post').appendChild(img_f);
+        console.log(qs('.input_img_event').value);
+        exit.addEventListener('click', (e)=>{
+            qs('.input_img_event').value = '';
+            img_f.remove();
+            exit.remove();
+        }, true)
         qs('.button--post--form').disabled = false;
         qs('.button--post--form').style.backgroundColor = '#53ffff';
         qs('.button--post--form').style.cursor = 'pointer';
