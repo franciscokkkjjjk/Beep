aec3cbc9388c8bdebf7a19d9ff6c6760b06834bd
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2022-09-06 22:01:58 -0300

    bobSponja

--- assets/script/php/interacoes_post/compartilhar_c_comentario.php
+++ assets/script/php/interacoes_post/compartilhar_c_comentario.php
@@ -2,6 +2,22 @@
 session_start(); 
 if(isset($_POST['cC_xd30'])) {
     $test_post = $_POST['cC_xd30'];
+    $arquivo_name  = $_FILES['midia_repost']['name'];
+    if($arquivo_name == '' or $arquivo_name == null) {
+        $name_banco = null;
+    } else {
+        $diretorio = '../../../imgs/posts/';
+        $ex = strtolower(pathinfo($arquivo_name,PATHINFO_EXTENSION));
+        $name_banco = bin2hex(random_bytes(20)) . '.' . $ex;
+        if(!move_uploaded_file($_FILES["midia_repost"]["tmp_name"], $diretorio.$name_banco)) {
+            $json = [
+                'moio' => true,
+                'error' => 'não deu update no arquivo'
+             ];
+             echo json_encode($json);
+             die;
+        }
+    }
 } else {
     echo 'saia agora daq';
 }
