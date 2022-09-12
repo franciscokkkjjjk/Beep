7bee9531b3a5b6796778823d9a9979e838d4065d
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2022-09-12 20:19:19 -0300

    bobSponja

--- paginas/jogos.php
+++ paginas/jogos.php
@@ -17,6 +17,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../assets/style/generic/style.css">
     <link rel="stylesheet" href="../assets/style/feed/style.css">
+    <link rel="stylesheet" href="../assets/style/jogos/style.css">
     <style>
         <?php 
             if(!$_SESSION['img'] == '' and !$_SESSION['img'] == null) {
@@ -37,13 +38,35 @@
             require_once '../assets/script/php/html__generic/nav_menu.php';
         ?>
         <div class="timeline--area">
-           ass
+            <div class="feed-header-body">
+                    <h1>
+                        lista de Jogos da Beep
+                    </h1>
+                    <div class="feed-logo-body menu--header">
+                        <div class="menu">
+                            <div class="menu--pag--button-menu--area " >
+                                <div class="menu--pag--button button--header">
+                                    <div class="event--header"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="menu--header--body">
+                            <a href='<?= pagAtual('caminho');?>../assets/script/php/logout.php' class="opt--menu-header">
+                                <div class="img--menu--header logout"></div>
+                                <div class="text--menu--header">logout</div>
+                            </a>
+                        </div>
+                </div>
+            </div>
+            <div class="feed-body-post">
+                <div class='area_jogo_body'>
+                    
+                </div>
+            </div>
         </div>
-        <?php 
-            require_once '../assets/script/php/html__generic/recomendado.php';
-        ?> 
     </div>
     
     <script type="text/javascript" src="../assets/script/javascript/default/script.js"></script>
+    <script type="text/javascript" src="../assets/script/javascript/default/event_header.js"></script>
 </body>
 </html>
