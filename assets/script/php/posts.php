3cd92e9f82e36a0bdb2aabd2cdcb543041251f7e
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2022-09-16 14:28:12 -0300

    bobSponja

--- assets/script/php/posts.php
+++ assets/script/php/posts.php
@@ -7,7 +7,7 @@
         $text_post = addslashes($_POST['post_text']);
     }
     if($img_perfil = $_FILES['img_post']['name'] == '') {
-        $img_post = NULL;
+        $nome_banco_perfil = NULL;
     } else {
         $diretorio = '../../imgs/posts/';
         $img_post = $_FILES['img_post']['name'];
@@ -27,8 +27,9 @@
     date_default_timezone_set('America/Sao_Paulo');
     date_default_timezone_get();
     $data_publi = date('Y-m-d H:i:s');
-    $sql_post = "INSERT INTO publicacoes(user_publi, type, id_publi_interagida, text_publi, img_publi, num_curtidas, num_compartilha, date_publi, num_comentario) VALUE (".$_SESSION['id_user'].",3, NULL,'$text_post','$novo_nome', 0, 0, '$data_publi', 0)";
+    $sql_post = "INSERT INTO publicacoes(user_publi, type, id_publi_interagida, text_publi, img_publi, num_curtidas, num_compartilha, date_publi, num_comentario, id_game) VALUE (".$_SESSION['id_user'].",3, NULL,'$text_post','$nome_banco_perfil', 0, 0, '$data_publi', 0, NULL)";
     $res_post = mysqli_query($conexao, $sql_post);
+    var_dump($res_post);
     if($res_post) {
         $_SESSION['menssagem'] = 'Postagem enviada com sucesso.';
         header('location:../../../paginas/inicial.php');
