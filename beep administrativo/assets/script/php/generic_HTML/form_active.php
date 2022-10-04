3cf901bc82e6a799f891f4e69ab0b8e3c7faab56
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2022-10-03 21:43:35 -0300

    bobSponja

---
+++ beep administrativo/assets/script/php/generic_HTML/form_active.php
@@ -0,0 +1,16 @@
+<div class="body_active">
+    <form method="POST">
+    <div class="area_input">
+        <div class="title_active">defina</div>
+        <div class="inputs_active">
+            <div class="senha_">
+                <input type='password' name='senha_'>
+            </div>
+            <div class="senha_confirm">
+                <input type='password' name='senha_'>
+            </div>
+            <button type="submit">Enviar</button>
+        </div>
+    </div>
+    </form>
+</div>
