9aaa2b055eb75e4b6912fcb35c95440da0a5d125
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2022-10-03 21:36:31 -0300

    bobSponja

--- beep administrativo/paginas/inicial.php
+++ beep administrativo/paginas/inicial.php
@@ -1,6 +1,7 @@
 <!-- <a href="../assets/script/php/logout_root.php">s</a> -->
 <?php 
-    if(!assert($_SESSION['id_root'])) {
+    session_start();
+    if(!isset($_SESSION['id_root'])) {
         header('location:../');
     }
 ?>
@@ -10,9 +11,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../../assets/imgs/default/beep_logo.png">
     <title>Inicial | Beep Administrativo</title>
 </head>
 <body>
-    
+    <?php 
+        if(isset($_SESSION['ative'])) {
+            require_once '../assets/script/php/generic_HTML/form_active.php';
+        } else {
+    ?>
+    teste
+    <?php
+        }
+    ?>
 </body>
 </html>
