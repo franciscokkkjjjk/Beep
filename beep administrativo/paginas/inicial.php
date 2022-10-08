005ea14412e8733b224a9bb55e616613d784325b
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2022-10-08 17:52:17 -0300

    bobSponja

--- beep administrativo/paginas/inicial.php
+++ beep administrativo/paginas/inicial.php
@@ -63,7 +63,31 @@
         </div>
     </header>
     <main>
-        
+        <div class="title_main">
+            Jogos
+        </div>
+        <div class="corpo_list">
+            <div class="list_area">
+                <div class="area_list_info_1">
+                    <div class="list_img">
+                    </div>
+                    <div class="list_title">
+                        Red Dead Redempition
+                    </div>
+                </div>
+                <div class="area_list_info_2">
+                    <div class="button_a button_int">
+                        <div class="img_"></div>
+                    </div>
+                    <div class="button_b button_int">
+                        <div class="img_"></div>
+                    </div>
+                    <div class="button_c button_int">
+                        <div class="img_"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </body>
 </html>
