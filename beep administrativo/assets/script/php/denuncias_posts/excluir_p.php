0b144bf39f3fe7a49e8b07183252154cafa3d3c3
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2022-11-14 10:47:05 -0300

    bobSponja

--- beep administrativo/assets/script/php/denuncias_posts/excluir_p.php
+++ beep administrativo/assets/script/php/denuncias_posts/excluir_p.php
@@ -1,8 +1,4 @@
 <?php
-echo 'em construção...';
-
-//mandei o ultimo por GET então esse tbm vai ser mandado por get
-//deve apenas excluir a publicação == as outras verficações serão feitas em outros arq
 if (isset($_GET['id_p'])) {
     session_start();
     require_once "../conect_pdo.php";
