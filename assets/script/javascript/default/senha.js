e5a80dafafca1cce7e6bfa34b007e3df48946538
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2022-12-03 07:27:16 -0300

    bobSponja

--- assets/script/javascript/default/senha.js
+++ assets/script/javascript/default/senha.js
@@ -3,7 +3,11 @@ document.querySelector('form').onsubmit = async (e) => {
     let form = document.querySelector('form');
     let value = new FormData(form);
     let res;
-    
+    document.querySelector('.loading_').style.display = '';
+    document.querySelector('.loading_').style.display = '';
+    document.querySelector('.aguar').style.display = '';
+    document.querySelector(".event").style.display = '';
+    document.querySelector('.foi').style.display = 'none';
     try {
         let req = await fetch("../assets/script/php/requsicoes/recuperar_senha.php", {
             method: "POST",
@@ -11,8 +15,39 @@ document.querySelector('form').onsubmit = async (e) => {
         })
         res = await req.json();
     } catch {
-
+        document.querySelector('.aguar').style.display = 'none';
+        document.querySelector(".event").style.display = 'none';
+        document.querySelector('.foi').innerHTML = "Email enviado com sucesso";
+        document.querySelector('.foi').style.display = '';
+        document.querySelector(".body_loading").style.width = 'auto';
+        document.querySelector(".body_loading").style.padding = '10px';
+        setTimeout(() => {
+            document.querySelector('.loading_').style.display = 'none';
+            document.querySelector('.aguar').style.display = '';
+            document.querySelector(".event").style.display = '';
+            document.querySelector('.foi').innerHTML = "";
+            document.querySelector('.foi').style.display = 'none';
+            document.querySelector(".body_loading").style.width = '';
+            document.querySelector(".body_loading").style.padding = '';
+        }, 2000)
         return;
     }
+    document.querySelector('.loading_').style.display = '';
+    document.querySelector('.aguar').style.display = 'none';
+    document.querySelector(".event").style.display = 'none';
+    console.log(res);
+    document.querySelector('.foi').innerHTML = res.mensage;
+    document.querySelector('.foi').style.display = '';
+    document.querySelector(".body_loading").style.width = 'auto';
+    document.querySelector(".body_loading").style.padding = '10px';
+    setTimeout(() => {
+        document.querySelector('.loading_').style.display = 'none';
+        document.querySelector('.aguar').style.display = '';
+        document.querySelector(".event").style.display = '';
+        document.querySelector('.foi').innerHTML = "";
+        document.querySelector('.foi').style.display = 'none';
+        document.querySelector(".body_loading").style.width = '';
+        document.querySelector(".body_loading").style.padding = '';
+    }, 2000)
     console.log(res);
 }
--- paginas/esqueceu_senha.php
+++ paginas/esqueceu_senha.php
@@ -1,15 +1,15 @@
-<?php 
-    session_start();
-    if(isset($_SESSION['id_user'])){
-        header('location:paginas/inicial.php');
-    } else {
+<?php
+session_start();
+if (isset($_SESSION['id_user'])) {
+    header('location:paginas/inicial.php');
+} else {
     $erro = '';
     $erro_email_s = '';
     $erroEmail = false;
     $erroLogin = false;
     $email = '';
-    if(isset($_SESSION['mensagem'])) {
-        if(isset($_SESSION['erro_email'])){
+    if (isset($_SESSION['mensagem'])) {
+        if (isset($_SESSION['erro_email'])) {
             $erroEmail = $_SESSION['erro_email'];
             $erro_email_s = 'error--login';
         } else {
@@ -22,10 +22,13 @@
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Chewy&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Chewy&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -38,60 +41,71 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="../assets/style/generic/style.css">
     <link rel="stylesheet" href="../assets/style/dream/style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
     <title>Login | Beep</title>
 </head>
+
 <body>
-<main>
-    <div class="container">
-        <div class="body--text">
-            <div class="area--text">
-                <div class="titulo--beep">
-                    <img src="../assets/imgs/default/Beep_logo_title.png">
-                </div>
-                <div class="text--beep">
-                    <p>
-                    Com a beep você pode se divertir com seus amigos, e conhecer pessoas novas. Além de ampliar sua perspectiva sobre jogos.
-                    </p>
+    <main>
+        <div class="container">
+            <div class="body--text">
+                <div class="area--text">
+                    <div class="titulo--beep">
+                        <img src="../assets/imgs/default/Beep_logo_title.png">
+                    </div>
+                    <div class="text--beep">
+                        <p>
+                            Com a beep você pode se divertir com seus amigos, e conhecer pessoas novas. Além de ampliar
+                            sua perspectiva sobre jogos.
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <form action="#" method="POST">
-        <div class="body--form">
-            <div class="form--area">
-                <div class="tutulo--area--form">
-                    Recuperar senha
-                </div>
-                <div class="input--area--form">
-                    <div class="input--form">
-                        <input id="email" required class="<?= $erro_email_s?>" value="<?=  $email?>" placeholder="Email" type="email" name="email">
-                    </div>
-                    <div class="mensagem--erro"></div>
-                    <?php 
-                        if($erroEmail) {
+            <form action="#" method="POST">
+                <div class="body--form">
+                    <div class="form--area">
+                        <div class="tutulo--area--form">
+                            Recuperar senha
+                        </div>
+                        <div class="input--area--form">
+                            <div class="input--form">
+                                <input id="email" required class="<?= $erro_email_s ?>" value="<?= $email ?>"
+                                    placeholder="Email" type="email" name="email">
+                            </div>
+                            <div class="mensagem--erro"></div>
+                            <?php
+                    if ($erroEmail) {
                     ?>
-                    <div class="mensagem--erro">
-                        <?php echo $_SESSION['mensagem'];
+                            <div class="mensagem--erro">
+                                <?php echo $_SESSION['mensagem'];
                         session_destroy();
                         ?>
-                    </div>
-                    <?php }?>
-                </div>
-                <div class="buttons--area">
-                    <div class="entrar--area">
-                        <button type="submit">Recuperar senha</button>
-                    </div>
-                    <div class="area--button--eC">
-                        <div class="button-C">
-                            <a href="../index.php">Voltar</a>
+                            </div>
+                            <?php } ?>
+                        </div>
+                        <div class="buttons--area">
+                            <div class="entrar--area">
+                                <button type="submit">Recuperar senha</button>
+                            </div>
+                            <div class="area--button--eC">
+                                <div class="button-C">
+                                    <a href="../index.php">Voltar</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                <div class="loading_" style="display:none">
+                    <div class="body_loading">
+                        <div class="event"></div>
+                        <div class="loding aguar">Aguarde</div>
+                        <div class="loding foi" style="display: none;"></div>
+                    </div>
+                </div>
+            </form>
         </div>
-                            
-    </form>
-    </div>
     </main>
 
     <!--Materialize
@@ -101,4 +115,5 @@
     <script type="text/javascript" src="../assets/script/javascript/default/script.js"></script>
     <script type="text/javascript" src="../assets/script/javascript/default/senha.js"></script>
 </body>
+
 </html>
