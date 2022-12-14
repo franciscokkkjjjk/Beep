8bcb60b61d249ce53544addcb5667f575b9269fb
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2022-12-14 10:34:19 -0300

    bobSponja

--- assets/script/javascript/default/form_creat.js
+++ assets/script/javascript/default/form_creat.js
@@ -474,20 +474,3 @@ let buttonEdit = qs('.button--editar');
 if (buttonEdit != null) { buttonEdit.addEventListener('click', creatFormEdit, true); }
 
 
-/*
-
-<canvas id='canvas'></canvas> para recortar imagem
-function cropImg(){
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
-
-  var image = new Image();
-  image.src = "https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=725&q=80"; 
-  
-  image.onload = function(){
-    ctx.drawImage(image, 150, 200, 500, 300, 60,60, 500, 300);
-  }
-}
-
-cropImg();
-*/
